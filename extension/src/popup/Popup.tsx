5629cda7f464dadeb2a9b5be37dfe18be16a6c1e
KARUNARATHNA W M D D <dilshankarunarathnastudy@gmail.com>
2026-06-12 23:17:51 +0530

    Content Script added!

--- extension/src/popup/Popup.tsx
+++ extension/src/popup/Popup.tsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
+
 function Popup() {
+  const [title, setTitile] = useState("");
+
+  const getPageTitle = async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+
+    if (!tab.id) return;
+
+    chrome.tabs.sendMessage(
+      tab.id,
+      { type: "GET_PAGE_TITLE" },
+      (response: { title?: string }) => {
+        if (response?.title) {
+          setTitile(response.title);
+        }
+      },
+    );
+  };
+
   return (
-    <div>
-      <h1>AI Post Generator</h1>
+    <div style={{ width: 350, padding: 16 }}>
+      <h2>AI Post Generator</h2>
 
-      <button>Generate LinkedIn Post</button>
+      <button onClick={getPageTitle}>Get Page Title</button>
 
-      <textarea placeholder="Generated content..." />
+      <hr />
+
+      <p>
+        <strong>Result:</strong>
+      </p>
+
+      <p>{title}</p>
     </div>
   );
 }
