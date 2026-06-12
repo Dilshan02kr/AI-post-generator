944dacca5fffc157652465813346b587f90684ce
KARUNARATHNA W M D D <dilshankarunarathnastudy@gmail.com>
2026-06-12 21:21:10 +0530

    Popup jsx code added

---
+++ extension/src/popup/Popup.tsx
@@ -0,0 +1,13 @@
+function Popup() {
+  return (
+    <div>
+      <h1>AI Post Generator</h1>
+
+      <button>Generate LinkedIn Post</button>
+
+      <textarea placeholder="Generated content..." />
+    </div>
+  );
+}
+
+export default Popup;
