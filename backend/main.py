1b590b6457f5f8a41f665abf1621e1cf26382bae
KARUNARATHNA W M D D <dilshankarunarathnastudy@gmail.com>
2026-06-22 11:50:22 +0530

    feat: add GenerateRequest model and implement generate endpoint in FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models.request_models import GenerateRequest
 
 app = FastAPI()
 
@@ -6,4 +7,14 @@ app = FastAPI()
 def home():
     return {
         "message": "Hello from AI Post Generator Backend"
+    }
+
+@app.post("/generate")
+def generate(request: GenerateRequest):
+    print(request.title)
+    print(request.content)
+    print(request.style)
+
+    return {
+        "post": "This is a generated post based on the provided title, content, and style."
     }
